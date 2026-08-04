825209d34789180579155d38b7741d6e98a49d0a
ANSH GUPTA <ansh1692005@gmail.com>
2026-08-04 18:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 32.4 MB (54.61%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            int curr = nums[i] + 1;
+
+            while (curr < nums[i + 1]) {
+                ans.push_back(curr);
+                curr++;
+            }
+        }
+
+        return ans;
+
+    }
+};
