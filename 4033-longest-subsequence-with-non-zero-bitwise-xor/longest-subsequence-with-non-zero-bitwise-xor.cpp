39f7fc0499b529c82a502668ec4233e6eb07ff66
ANSH GUPTA <ansh1692005@gmail.com>
2026-08-15 10:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 171.2 MB (88.49%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
+        int n = nums.size();
         int totalXor = 0;
-        bool hasNonZero = false;
+        for (int x : nums) {
+            totalXor ^= x;
+        }
         
-        for (int num : nums) {
-            totalXor ^= num;
-            if (num != 0) {
-                hasNonZero = true;
+        if (totalXor != 0) {
+            return n;
+        }
+        
+        // totalXor == 0
+        for (int x : nums) {
+            if (x != 0) {
+                return n - 1;
             }
         }
         
-        // If the XOR of all elements is non-zero, the whole array is the answer.
-        if (totalXor != 0) {
-            return nums.size();
-        }
-        
-        // If the total XOR is 0 but there is at least one non-zero element,
-        // we can remove exactly one non-zero element to make the remaining XOR non-zero.
-        if (hasNonZero) {
-            return nums.size() - 1;
-        }
-        
-        // If all elements are 0, it's impossible to get a non-zero XOR.
         return 0;
     }
 };
