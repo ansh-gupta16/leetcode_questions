0a58b6a148b4debc00164c9f25dd537d6771504a
ANSH GUPTA <ansh1692005@gmail.com>
2026-09-04 16:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (56.73%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefixMax(n), suffixMin(n);
-        
-        prefixMax[0] = nums[0];
-        for (int i = 1; i < n; i++)
-            prefixMax[i] = max(prefixMax[i-1], nums[i]);
+        vector<int> suffixMin(n);
         
         suffixMin[n-1] = nums[n-1];
         for (int i = n-2; i >= 0; i--)
             suffixMin[i] = min(suffixMin[i+1], nums[i]);
         
+        int prefixMax = nums[0];
         for (int i = 0; i < n; i++) {
-            if (prefixMax[i] - suffixMin[i] <= k)
+            prefixMax = max(prefixMax, nums[i]);
+            if (prefixMax - suffixMin[i] <= k)
                 return i;
         }
         
